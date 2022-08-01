be33d3f773dd18a6bf11cfc3bb247c5405d163a9
Angira Kekteeva <kira@nspcc.ru>
2022-08-02 00:47:20 +0400

    [#186] Don't print env vars with empty values
    Signed-off-by: Angira Kekteeva <kira@nspcc.ru>

--- settings.go
+++ settings.go
@@ -182,8 +182,13 @@ func settings() *viper.Viper {
 				continue
 			}
 
+			defaultValue := v.GetString(keys[i])
+			if len(defaultValue) == 0 {
+				continue
+			}
+
 			k := strings.Replace(keys[i], ".", "_", -1)
-			fmt.Printf("%s_%s = %v\n", Prefix, strings.ToUpper(k), v.Get(keys[i]))
+			fmt.Printf("%s_%s = %s\n", Prefix, strings.ToUpper(k), defaultValue)
 		}
 
 		fmt.Println()
